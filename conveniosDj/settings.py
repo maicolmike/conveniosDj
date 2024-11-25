adc64023549c0afbbe3e394130761730ca016bbb
Maicol <maicolmike@hotmail.com>
2024-11-25 16:51:18 -0500

    primer paso

--- conveniosDj/settings.py
+++ conveniosDj/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'conveniosDj.urls'
+
+AUTH_USER_MODEL = 'users.User' #con esto ya no utilizamos el user del sistema sino el nuestro
 
 TEMPLATES = [
     {

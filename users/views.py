76d88a1251cacfb13dba47fffbd6406c9a2a4201
Maicol <maicolmike@hotmail.com>
2024-11-25 17:28:17 -0500

    primer paso

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+#Inicio de sesion login
+def login_view(request):
+    return render(request, 'users/login.html', {
+        'title': "Login",
+        
+    })

f4bed30515b00bbfa43df7d1952afea31cadb7ac
Maicol <maicolmike@hotmail.com>
2024-11-26 08:19:49 -0500

    nav bar

--- users/views.py
+++ users/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
+from .models import User
+from .forms import LoginUser
+from django.contrib.auth import login,logout,authenticate
+from django.contrib import messages
+from django.shortcuts import render,redirect,get_object_or_404
 
 # Create your views here.
 #Inicio de sesion login
 def login_view(request):
+    if request.method == 'POST':
+        form = LoginUser(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                #messages.success(request, 'Bienvenido {}'.format(user.username))
+                messages.success(request, 'Bienvenido {}'.format(user.username))
+                return redirect('index')  # Redirige al usuario a la página de inicio
+            else:
+                messages.error(request, 'Usuario o contraseña incorrectos')
+    else:
+        form = LoginUser()  # Crea un formulario vacío si la solicitud no es POST
+    
+    # Si el usuario ya está autenticado, redirige a la página de inicio
+    if request.user.is_authenticated:
+        return redirect('index')
+
     return render(request, 'users/login.html', {
         'title': "Login",
-        
+        'form': form
     })
+

d4162c7318c90d103b4ca16b788d6db2ee6c2742
Maicol <maicolmike@hotmail.com>
2024-11-27 10:34:54 -0500

    nav bar

--- consulta/views.py
+++ consulta/views.py
@@ -3,11 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required # vista basada en funciones que no permita acceder a paginas donde no se ha logeado
-
+from .forms import ConsultaAsociado
 
 @login_required(login_url='login')
 def consulta(request):
     #return HttpResponse('Hola mundo')
+    form = ConsultaAsociado(request.POST or None)
     return render(request, 'consulta/consulta.html',{ 
         'title': "Consultar asociado",
+        'form': form,
     })
